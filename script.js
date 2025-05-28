a4045520e2d3908a210720e6dbac40aaefc06ea0
Kauan-JuniorDev <kauan.silva28alunos@gmail.com>
2025-05-28 20:51:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var alerta;
-alerta = "ALERTA! CORNO NO SITE KKKKKKKKK"
+alerta = "ALERTA! CORNO DETECTADO NO SITE KKKKKKKKKKKK";
 
 alert (alerta);
+
+var texto_repetido = 'texto_repetido';
+
+var numero_repetido = 0;
+
+while (numero_repetido < 10);
+{
+    alert (texto_repetido + numero_repetido);
+    numero_repetido = numero_repetido +1;
+};
