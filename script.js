ef510c79f8c0aa7cb86fcfd9fefc377493f60bd8
Kauan-JuniorDev <kauan.silva28alunos@gmail.com>
2025-05-28 20:59:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var alerta;
-alerta = "ALERTA! CORNO DETECTADO NO SITE KKKKKKKKKKKK";
-
-alert (alerta);
-
 var texto_repetido = 'texto_repetido';
 
 var numero_repetido = 0;
@@ -12,3 +7,7 @@ while (numero_repetido < 10);
     alert (texto_repetido + numero_repetido);
     numero_repetido = numero_repetido +1;
 };
+
+var alerta;
+alerta = "ALERTA! CORNO DETECTADO NO SITE KKKKKKKKKKKK";
+alert (alerta);
