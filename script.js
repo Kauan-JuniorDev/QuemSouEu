cd5e05de8a0ba1d2f266434b233479fbb7d5d15c
Kauan-JuniorDev <kauan.silva28alunos@gmail.com>
2025-06-18 21:00:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,15 @@ for (let numero_repetido = 0; numero_repetido < 10; numero_repetido++)
 {
   console.log(texto_repetido + " - " + numero_repetido);
 }
-
 var alerta;
 alerta = "alerta";
 alert (alerta);
-
 var informação = document.getElementById("button_0001");
 informação.addEventListener('click', function () {
-  alert('informação');
-});
-
+  alert('informação');});
 var aviso = document.getElementById("button_0002");
 aviso.addEventListener('click', function () {
-  alert('aviso');
-});
-
+  alert('aviso');});
 var perigo = document.getElementById("button_0003");
 perigo.addEventListener('click', function () {
-  alert('perigo');
-});
+  alert('perigo');});
